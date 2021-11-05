475bb6d76df603070fe08b1c803fdd7e016fbfd9
Isabela Cardoso <isabela.cardoso@ibama.gov.br>
2021-11-05 13:54:50 -0300

    raspagem

--- scripts/get-data.js
+++ scripts/get-data.js
@@ -1,18 +1,58 @@
 function setData() {
-  const title = document.querySelectorAll("#conteudo .resultado");
+  const results = document.querySelectorAll("#conteudo .resultado");
+  const body = document.querySelector("body");
+  const infos = document.querySelectorAll(".ancoraSigla")
+  // const metadata = document.querySelectorAll("#conteudo .resultado .metatag tr")
 
-  const logArrayElements(element, index, title) {
-    console.log("a[" + index + "] = " + element);
-  }
+  
 
-  title.forEach(logArrayElements);
+  // results.forEach( (result) => {
+  //   console.log(result.innerText);
+  //   lista.insertAdjacentHTML("beforeend", `<p>${result.innerText}</p>`)
+  // });
 
-  // const url = window.location.href;
+  body.insertAdjacentHTML("beforeend",
+    `<table>
+      <thead>
+      <tr>
+        <th>Tipo</th>
+        <th>Nº</th>
+        <th>Assunto</th>
+        <th>Data</th>
+        <th>Unidade Geradora</th>
+        <th>Usuário Gerador</th>
+      </tr>
+      <thead>
+      <tbody>
+          <div id="p-sei-type"></div>
+          <td id="number"></td>
+          <td id="subject"></td>
+          <td id="date"></td>
+          <td id="p-sei-unity"></td>
+          <div id="user"></div>
+      </tbody>
+    </table>`
 
-  // return {
-  //   title: title,
-  //   // url: url
-  // }
+  )
+
+  const colType = document.querySelector("#p-sei-unity")
+  const colUser = document.querySelector("#user")
+
+  console.log(colType);
+
+  infos.forEach( (element, index) => {
+    if (index % 2 === 0) {
+      colType.insertAdjacentHTML("beforeend", `<p>${element.innerText}</p>`)
+    } else {
+      colUser.insertAdjacentHTML("beforeend", `<td>${element.innerText}</td>`)
+    }
+  });
+
+  
+
 }
 
+
+
 setData();
+
