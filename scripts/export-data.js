7d66f24b4eb6a37021073a685d641ef33e60d933
Daniel Perini <dsperini@gmail.com>
2021-11-11 12:57:41 -0300

    export done

--- scripts/export-data.js
+++ scripts/export-data.js
@@ -1,35 +1,37 @@
 // teste
 export const exportData = () => {
 
+  // precisa pegar o total passado pela função total-results.js
+  // totalResults
+  const total = parseInt(document.getElementById("numberResults").innerText, 10);
 
-// precisa pegar o total passado pela função total-results.js
-const total = 20
-    // take elements
-    const seiType = document.getElementById("p-sei-type");
-    const seiNumber = document.getElementById("p-sei-number");
-    const seiSubject = document.getElementById("p-sei-subject");
-    const seiDate = document.getElementById("p-sei-date");
-    const seiUnity = document.getElementById("p-sei-unity");
-    const seiUser = document.getElementById("p-sei-user");
+  console.log(total);
+      // take elements
+      const seiType = document.getElementById("p-sei-type");
+      const seiNumber = document.getElementById("p-sei-number");
+      const seiSubject = document.getElementById("p-sei-subject");
+      const seiDate = document.getElementById("p-sei-date");
+      const seiUnity = document.getElementById("p-sei-unity");
+      const seiUser = document.getElementById("p-sei-user");
 
-    // elements change to array
-    const seiTypeArray = seiType.innerText.split("\n\n");
-    const seiNumberArray = seiNumber.innerText.split("\n\n");
-    const seiSubjectArray = seiSubject.innerText.split("\n\n");
-    const seiDateArray = seiDate.innerText.split("\n\n");
-    const seiUnityArray = seiUnity.innerText.split("\n\n");
-    const seiUserArray = seiUser.innerText.split("\n\n");
+      // elements change to array
+      const seiTypeArray = seiType.innerText.split("\n\n");
+      const seiNumberArray = seiNumber.innerText.split("\n\n");
+      const seiSubjectArray = seiSubject.innerText.split("\n\n");
+      const seiDateArray = seiDate.innerText.split("\n\n");
+      const seiUnityArray = seiUnity.innerText.split("\n\n");
+      const seiUserArray = seiUser.innerText.split("\n\n");
 
-    // init new array
-    const rows = [
-     ["Tipo", "Número", "Assunto", "Data", "Unidade", "Usuário"],
-    ];
+      // init new array
+      const rows = [
+        ["Tipo", "Número", "Assunto", "Data", "Unidade", "Usuário"],
+      ];
 
-    // count to iterator take specific element
-    let idCount = 0;
+      // count to iterator take specific element
+      let idCount = 0;
       while (idCount < total) {
 
-        // 
+        //
         const seiTypeElement = seiTypeArray[`${idCount}`];
         const seiNumberElement = seiNumberArray[`${idCount}`];
         const seiSubjecElement = seiSubjectArray[`${idCount}`];
@@ -38,32 +40,32 @@ const total = 20
         const seiUserElement = seiUserArray[`${idCount}`];
 
         // append new line
-        rows.push([`${seiTypeElement}`, 
-                 `${seiNumberElement}`,
-                 `${seiSubjecElement}`, 
-                 `${seiDateElement}`, 
-                 `${seiUnityElement}`, 
-                 `${seiUserElement}`]
-               )
-        
+        rows.push([`${seiTypeElement}`,
+        `${seiNumberElement}`,
+        `${seiSubjecElement}`,
+        `${seiDateElement}`,
+        `${seiUnityElement}`,
+        `${seiUserElement}`]
+        )
+
         // increment count
         idCount++;
-    }
-     
+      }
 
-    let csvContent = "data:text/csv;charset=utf-8,";
 
-    rows.forEach(function(rowArray) {
-      let row = rowArray.join(";");
-      csvContent += row + "\r\n";
-    });
+      let csvContent = "data:text/csv;charset=utf-8,";
 
-    const constEncodedUri = encodeURI(csvContent);
+      rows.forEach(function (rowArray) {
+        let row = rowArray.join(";");
+        csvContent += row + "\r\n";
+      });
 
-    const link = document.createElement("a");
-    link.textContent = "Save as CSV";
-    link.download = "file.csv";
+      const constEncodedUri = encodeURI(csvContent);
 
-    link.href = `${constEncodedUri}`;
-    
-  };
+      const link = document.createElement("a");
+      link.textContent = "Save as CSV";
+      link.download = "file.csv";
+      link.href = `${constEncodedUri}`;
+      link.click()
+
+};
