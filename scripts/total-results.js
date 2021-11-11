055bacdfda37f4b522f85cd38f6b63f160e133e6
Isabela Cardoso <isabela.cardoso@ibama.gov.br>
2021-11-11 11:00:22 -0300

    fetch com formData

--- scripts/total-results.js
+++ scripts/total-results.js
@@ -1,11 +1,16 @@
 
 function totalResults() {
+
+  // identifica a pagina ativa de resultados
+  let activePage = parseInt(document.querySelector(".paginas b").innerText);
   
   // identifica o total de resultados encontrados na pesquisa
   const total = parseInt(document.querySelectorAll("#conteudo .barra")[0].innerText.split(" ")[5].split("T")[0]);
   
   // identifica o total páginas
   const totalPages = Math.trunc(total/10) + 1;
+
+
 
   // retorna total de resultados e total de páginas
   return [total, totalPages]
