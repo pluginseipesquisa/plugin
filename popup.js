072d5d1e7eaa4893c4c5ac4eb64c80d751a070cd
Isabela Cardoso <isabela.cardoso@ibama.gov.br>
2021-11-12 11:24:38 -0300

    loading done

--- popup.js
+++ popup.js
@@ -6,134 +6,152 @@ function listenClick() {
   // scripts executados quando acionado ao click do btn Carregar Dados
   const button = document.getElementById('get-data');
   button.addEventListener('click', () => {
-
+	
     // script executado para buscar os parâmetros do formData
     chrome.tabs.executeScript({
       file: 'scripts/form-data.js'
     }, function (resultsFormParams) {
-      console.log(resultsFormParams);
+      console.log(resultsFormParams[0]);
 
       // script executado para retornar o numero de paginas e resultados e apresentar na popup
-      chrome.tabs.executeScript({
-          file: 'scripts/total-results.js'
-      }, function (resultsTotal) {
-        console.log(getTotal(resultsTotal));
-        if(resultsTotal[0]==null){
-          alert('sem resultados');
-          return
-        }  
-        // armazena o total de resultados e de paginas para realizar a iteração
-        const totalResults = parseInt(getTotal(resultsTotal));
-        const nPagesSei = parseInt(totalResults);
-        const totalPages = Math.trunc(totalResults/10);
 
-        // seleciona os elementos no html da popup
-        const resultsSei = document.querySelector("#results");
-        const pagesSei = document.querySelector("#pages");
+      //console.log(getTotal(resultsFormParams[0][2]));
+      if(resultsFormParams[0][2]==null){
+        alert('sem resultados');
+        return
+      }  
+      
+      
+      let resultsTotal = JSON.parse(resultsFormParams[0][2]);
+      // console.log('aqui1', resultsTotal)
+      
+      // armazena o total de resultados e de paginas para realizar a iteração
+      const totalResults = parseInt(getTotal(resultsTotal));
+      // console.log('aqui2', totalResults)
+      const nPagesSei = parseInt(totalResults);
+      const totalPages = Math.trunc(totalResults/10) + 1;
 
-        // insere os valores de total de resultados e páginas no html da popup
-        resultsSei.insertAdjacentHTML("beforeend", `<div id="numberResults">${totalResults}</div>`);
-        pagesSei.insertAdjacentHTML("beforeend", `<div>${totalPages} </div>`);
+      // seleciona os elementos no html da popup
+      const resultsSei = document.querySelector("#results");
+      const pagesSei = document.querySelector("#pages");
+      
+      resultsSei.innerHTML="";
+      pagesSei.innerHTML="";
+      
 
-      	//seleciona a div para inserir template e resultados
-        const popup = document.querySelector("#janela-popup");
+      // insere os valores de total de resultados e páginas no html da popup
+      resultsSei.insertAdjacentHTML("beforeend", `<div id="numberResults">${totalResults}</div>`);
+      pagesSei.insertAdjacentHTML("beforeend", `<div>${totalPages} </div>`);
 
-        //insere template no html da popup
-        popup.insertAdjacentHTML("beforeend",
-          `<div id="table-results" class="grid-container">
-            <div class="columnH"  id="Tipo">Tipo</div>
-            <div class="columnH" id="Numero">Número</div>
-            <div class="columnH" id="Assunto">Assunto</div>
-            <div class="columnH" id="Data">Data</div>
-            <div class="columnH" id="Unidade">Unidade</div>
-            <div class="columnH" id="Usuario">Usuário</div>
-            <div class="column border" id="p-sei-type"></div>
-            <div class="column border" id="p-sei-number"></div>
-            <div class="column border" id="p-sei-subject"></div>
-            <div class="column border" id="p-sei-date"></div>
-            <div class="column border" id="p-sei-unity"></div>
-            <div class="column border" id="p-sei-user"></div>
-          </div>`);
+    	//seleciona a div para inserir template e resultados
+      const popup = document.querySelector("#janela-popup");
+      popup.innerHTML = "";
+      
+      //insere template no html da popup
+      popup.insertAdjacentHTML("beforeend",
+        `<div id="table-results" class="grid-container">
+          <div class="columnH"  id="Tipo">Tipo</div>
+          <div class="columnH" id="Numero">Número</div>
+          <div class="columnH" id="Assunto">Assunto</div>
+          <div class="columnH" id="Data">Data</div>
+          <div class="columnH" id="Unidade">Unidade</div>
+          <div class="columnH" id="Usuario">Usuário</div>
+          <div class="column border" id="p-sei-type"></div>
+          <div class="column border" id="p-sei-number"></div>
+          <div class="column border" id="p-sei-subject"></div>
+          <div class="column border" id="p-sei-date"></div>
+          <div class="column border" id="p-sei-unity"></div>
+          <div class="column border" id="p-sei-user"></div>
+        </div>`);
 
-        // prepara os itens html da popup que recebem os resultados
-        const colType = document.querySelector("#p-sei-type");
-        const colNumber = document.querySelector("#p-sei-number");
-        const colSubject = document.querySelector("#p-sei-subject");
-        const colDate = document.querySelector("#p-sei-date");
-        const colDepartment = document.querySelector("#p-sei-unity")
-        const colUser = document.querySelector("#p-sei-user")
+      // prepara os itens html da popup que recebem os resultados
+      const colType = document.querySelector("#p-sei-type");
+      const colNumber = document.querySelector("#p-sei-number");
+      const colSubject = document.querySelector("#p-sei-subject");
+      const colDate = document.querySelector("#p-sei-date");
+      const colDepartment = document.querySelector("#p-sei-unity")
+      const colUser = document.querySelector("#p-sei-user")
 
-        // recebe os parâmetros do form
-        const formParams = resultsFormParams[0][1];
-        //console.log(formParams)
+      // recebe os parâmetros do form
+      const formParams = resultsFormParams[0][1];
+      //console.log(formParams)
 
-        // converte a string para JSON
-        let formJson = JSON.parse(formParams);
+      // converte a string para JSON
+      let formJson = JSON.parse(formParams);
 
-        for (var i=0; i < nPagesSei; i++) {
+      document.getElementById('loading').style.display = 'block';
+      for (var i=0; i < nPagesSei; i++) {
 
-          // atualiza o parâmetro
-        	formJson.hdnInicio = i*10;
-        	// console.log(i)
-        	//console.log(frmDt)
+        // atualiza o parâmetro
+      	formJson.hdnInicio = i*10;
+      	// console.log(i)
+      	//console.log(frmDt)
 
-          // monta um novo form
-        	let formSei = new FormData();
-          // insere os parametros atualizados
-        	for ( var key in formJson ) {
-            formSei.append(key, formJson[key]);
-        	}
+        // monta um novo form
+      	let formSei = new FormData();
+        // insere os parametros atualizados
+      	for ( var key in formJson ) {
+          formSei.append(key, formJson[key]);
+      	}
 
-        	// busca os resultados com a URL retornada e o form atualizado
-          fetch(resultsFormParams[0][0], {
-            method: 'POST',
-            body: formSei,
-         }).then(
-            response => response.arrayBuffer()
-         ).then(buffer => {
-              let decoder = new TextDecoder("iso-8859-1");
-              let text = decoder.decode(buffer);
-              const parser = new DOMParser();
-              const htmlDocument = parser.parseFromString(text, "text/html");
+      	// busca os resultados com a URL retornada e o form atualizado
+        fetch(resultsFormParams[0][0], {
+          method: 'POST',
+          body: formSei,
+        }).then(
+          response => response.arrayBuffer()
+        ).then(buffer => {
+        	//console.log()
+            let decoder = new TextDecoder("iso-8859-1");
+            let text = decoder.decode(buffer);
+            const parser = new DOMParser();
+            const htmlDocument = parser.parseFromString(text, "text/html");
+            
+            const section = htmlDocument.documentElement.querySelectorAll("#conteudo");
 
-              const section = htmlDocument.documentElement.querySelectorAll("#conteudo");
+            //const tables = section.querySelectorAll(".resultado")
+            let tables = section[0].querySelectorAll(".resultado");
 
-              //const tables = section.querySelectorAll(".resultado")
-              let tables = section[0].querySelectorAll(".resultado");
+            tables.forEach(function(data){
+              typeProcNumber = data.querySelectorAll(".resTituloEsquerda")[0].innerText;
+              numDoc = i + " - "+ data.querySelectorAll(".resTituloDireita")[0].innerText;
+              subject = data.querySelectorAll(".resSnippet")[0].innerText;
+              department = data.querySelectorAll(".ancoraSigla")[0].outerHTML;
+              user = data.querySelectorAll(".ancoraSigla")[1].outerHTML;
+              date = data.querySelectorAll(".metatag tr")[0].lastElementChild.innerText.split(" ")[1];
 
-              tables.forEach(function(data){
-                typeProcNumber = data.querySelectorAll(".resTituloEsquerda")[0].innerText;
-                numDoc = data.querySelectorAll(".resTituloDireita")[0].innerText;
-                subject = data.querySelectorAll(".resSnippet")[0].innerText;
-                department = data.querySelectorAll(".ancoraSigla")[0].outerHTML;
-                user = data.querySelectorAll(".ancoraSigla")[1].outerHTML;
-                date = data.querySelectorAll(".metatag tr")[0].lastElementChild.innerText.split(" ")[1];
+              html = `<p>${typeProcNumber}</p>`
+              colType.insertAdjacentHTML("beforeend", html);
 
-                html = `<p>${typeProcNumber}</p>`
-                colType.insertAdjacentHTML("beforeend", html);
+              html = `<p>${numDoc}</p>`
+              colNumber.insertAdjacentHTML("beforeend", html);
 
-                html = `<p>${numDoc}</p>`
-                colNumber.insertAdjacentHTML("beforeend", html);
+              html = `<p>${subject}</p>`
+              colSubject.insertAdjacentHTML("beforeend", html);
 
-                html = `<p>${subject}</p>`
-                colSubject.insertAdjacentHTML("beforeend", html);
+              html = `<p>${department}</p>`
+              colDepartment.insertAdjacentHTML("beforeend", html);
 
-                html = `<p>${department}</p>`
-                colDepartment.insertAdjacentHTML("beforeend", html);
+              html = `<p>${user}</p>`
+              colUser.insertAdjacentHTML("beforeend", html);
 
-                html = `<p>${user}</p>`
-                colUser.insertAdjacentHTML("beforeend", html);
+              html = `<p>${date}</p>`
+              colDate.insertAdjacentHTML("beforeend", html);
 
-                html = `<p>${date}</p>`
-                colDate.insertAdjacentHTML("beforeend", html);
-
-              });
-          });
-        };
-      });
+            });
+            
+            //console.log(parseInt(formSei.get('hdnInicio')),(nPagesSei-1) * 10)
+            if(parseInt(formSei.get('hdnInicio')) == (nPagesSei-1) * 10){
+            	document.getElementById('loading').style.display = 'none';
+            }
+        });
+      };
+      
+    
     });
     function getTotal(resultsTotal){
-      sptTotal = resultsTotal[0].split(" ");
+      sptTotal = resultsTotal.split(" ");
+      
       var total = 0;
       isPen = false;
       
@@ -179,3 +197,5 @@ const listenExport = () => {
 };
 
 listenExport();
+
+
